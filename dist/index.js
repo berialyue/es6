b94a47d11995e2d6de1c5d957a0a1b6c6d623c4c
berialyue <berialyue@gmail.com>
2018-07-02 15:28:08 +0800

    var let const

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,8 @@
 "use strict";
 
-var a = 1;
-console.log(a);
+var i = 1;
+for (var _i = 0; _i < 10; _i++) {
+  console.log("\u5FAA\u73AF\u4F53\u4E2D" + _i);
+}
+
+console.log("\u5FAA\u73AF\u4F53\u5916" + i);
--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
-let a = 1;
-console.log(a);
+const a = "berialyue"
+
+
+var i = 1;
+for(let i = 0;i<10;i++){
+  console.log(`循环体中${i}`)
+}
+
+console.log(`循环体外${i}`)
