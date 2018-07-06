582c5d5d863af082701497ff0cfb445c74f1aa77
berialyue <berialyue@gmail.com>
2018-07-07 00:34:13 +0800

    string-temp finished

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 // const a = "berialyue"
 
@@ -51,37 +51,28 @@
 // console.log(arr1)
 // console.log(arr2)
 
-function es(first) {
-  for (var _len = arguments.length, arg = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
-    arg[_key - 1] = arguments[_key];
-  }
+// function es(first, ...arg) {
+//   console.log(arg.length);
+//   console.log(arg);
+//   for(let val of arg){
+//     console.log(val);
+//   }
+// }
 
-  console.log(arg.length);
-  console.log(arg);
-  var _iteratorNormalCompletion = true;
-  var _didIteratorError = false;
-  var _iteratorError = undefined;
+// es(0,1,2,3,4,5,6,7,8)
 
-  try {
-    for (var _iterator = arg[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-      var val = _step.value;
+// let a = 'berialyue1111';
+// let b = `yueduming${a}`;
+// let c =1;
+// let d = 2;
+// let result = `${c+d}`
+// document.write(b)
+// document.write(result)
 
-      console.log(val);
-    }
-  } catch (err) {
-    _didIteratorError = true;
-    _iteratorError = err;
-  } finally {
-    try {
-      if (!_iteratorNormalCompletion && _iterator.return) {
-        _iterator.return();
-      }
-    } finally {
-      if (_didIteratorError) {
-        throw _iteratorError;
-      }
-    }
-  }
-}
+// let a = 'berialyue'
 
-es(0, 1, 2, 3, 4, 5, 6, 7, 8);
+// let b = 'yuedumingyuedumingberialyueyuedumingberialyue'
+
+// document.write(b.endsWith(a))
+
+document.write('berialyue | '.repeat(9));
